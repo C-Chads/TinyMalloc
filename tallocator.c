43b6226ff36859b8b9d344acc0c964c35b3d0cfb
gek <6974902+gordonfreeman424@users.noreply.github.com>
2021-07-17 21:40:30 -0400

    Automatic commit.

--- tallocator.c
+++ tallocator.c
@@ -26,8 +26,6 @@ static void tiny_mark_blocks(size_t start, size_t nblocks){
 		tallocator_metadata[start>>2] |= (unsigned char)(1<<((start&3)*2));
 	}
 }
-
-
 static void tiny_unmark_blocks(size_t i){
 	char have_started = 0;
 	for(;;){
@@ -50,10 +48,8 @@ void* tiny_malloc(size_t nbytes){
 	if(nbytes == 0) return NULL;
 	TALLOCATOR_MUTEX_LOCK
 	for(i = 0; i < (MANAGED_SIZE / BLOCK_SIZE); i++){
-		if(  (tallocator_metadata[i/4] & (3<<((i&3)*2))) == 0)
-			current_len++;
-		else /*Occupied!*/
-			current_len = 0;
+		if((tallocator_metadata[i/4] & (3<<((i&3)*2))) == 0) current_len++;
+		else /*Occupied!*/ current_len = 0;
 		if(current_len == nbytes) { /*Success!*/
 			size_t start = i - (current_len-1);
 			tiny_mark_blocks(start, current_len);
@@ -66,19 +62,15 @@ void* tiny_malloc(size_t nbytes){
 	TALLOCATOR_MUTEX_UNLOCK
 	return NULL;
 }
-
 int tiny_free(void* ptr){
-	size_t i = (size_t)ptr;
-	if(i == 0) return 0;
-	if ((unsigned char*)ptr < (MANAGED_BUFFER_PTR)) return 0;
+	size_t i = (size_t)ptr; if(i == 0) return 0;
+	if (i < (size_t)MANAGED_BUFFER_PTR) return 0;
 	i -= (size_t)MANAGED_BUFFER_PTR;
-	if(i % BLOCK_SIZE) return 0;
+	if(i % BLOCK_SIZE) return 0; /*This was not a pointer we handed out.*/
 	i /= BLOCK_SIZE;
 	TALLOCATOR_MUTEX_LOCK
 	if(  ((tallocator_metadata[i/4] & (3<<((i&3)*2)) )>>((i&3)*2)) == 3){
-		tiny_unmark_blocks(i);
-		TALLOCATOR_MUTEX_UNLOCK
-		return 1;
+		tiny_unmark_blocks(i); TALLOCATOR_MUTEX_UNLOCK; return 1;
 	}
 	TALLOCATOR_MUTEX_UNLOCK
 	return 0;
