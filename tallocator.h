d66fa40dcef6ae059f26253e4a53824efd8ae8ed
gek <6974902+gordonfreeman424@users.noreply.github.com>
2021-07-17 21:42:36 -0400

    Automatic commit.

--- tallocator.h
+++ tallocator.h
@@ -1,6 +1,6 @@
 #include <stddef.h>
 /*4 kilobytes block size*/
-#define BLOCK_SIZE 4096
+#define BLOCK_SIZE 0x1000
 /*The managed buffer's size. For speed, this should be a compiletime constant.*/
 #define MANAGED_SIZE ((size_t)BLOCK_SIZE * 1024 * 1024)
 void* tiny_malloc(size_t nbytes);
